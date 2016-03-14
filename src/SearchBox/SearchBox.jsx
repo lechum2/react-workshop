8777f62f61a70b59246aa2cb6c4508b66429dea4
Sebastian Lech <s.lech@hypermedia.pl>
2016-03-14 12:24:41 +0100

    Pass serch value as props

--- src/SearchBox/SearchBox.jsx
+++ src/SearchBox/SearchBox.jsx
@@ -9,11 +9,23 @@ import React from "react";
 import Result from "src/SearchBox/Result";
 
 export default React.createClass({
+    "getInitialState": function () {
+        return {
+            "SearchValue": "No search value"
+        };
+    },
+    "handleChange": function (evt) {
+        this.setState({
+            "SearchValue": evt.target.value
+        });
+        //this.state.SearchValue = evt.target.value;
+        //console.log(evt.target.value);
+    },
     "render": function () {
         return (
             <div>
-                <input type="text" />
-                <Result />
+                <input onChange={this.handleChange} type="text" />
+                <Result text={this.state.SearchValue} />
             </div>
         );
     }
